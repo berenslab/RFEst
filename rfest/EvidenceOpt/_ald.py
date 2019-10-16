ea8be435fd957f528382db201d0e1c63fee4bb94
Ziwei Huang <huang-ziwei@outlook.com>
2019-10-16 09:53:16 +0200

    update print_progress

--- rfest/EvidenceOpt/_ald.py
+++ rfest/EvidenceOpt/_ald.py
@@ -63,12 +63,27 @@ class ALD(EmpiricalBayes):
 
         return C, C_inv
 
-    def print_progress_header(self):
-        print('Iter\tσ\tρ\tτs_t\tτf_t\tτs_y\tτf_y\tτs_x\tτf_x\tcost')
-
     def print_progress(self, i, params, cost):
         print('{0:4d}\t{1:1.3f}\t{2:1.3f}\t{3:1.3f}\t{4:1.3f}\t{5:1.3f}\t{6:1.3f}\t{7:1.3f}\t{8:1.3f}\t{9:1.3f}'.format(
-        i, params[0], params[1], 
-           params[2], params[4],
-           params[6], params[8],
-           params[10],params[12], cost))   
+        i, params[0], params[1], params[2], params[4], params[6], params[8], params[10], params[12], cost))   
+        
+    def print_progress_header(self, params):
+        
+        if len(params) == 6:
+            print('Iter\tσ\tρ\tτs_t\tτf_t\tcost')
+        elif len(params) == 10:
+            print('Iter\tσ\tρ\tτs_t\tτf_t\tτs_y\tτf_y\tcost')
+        elif len(params) == 14:
+            print('Iter\tσ\tρ\tτs_t\tτf_t\tτs_y\tτf_y\tτs_x\tτf_x\tcost')
+
+    def print_progress(self, i, params, cost):
+     
+        if len(params) == 3:
+            print('{0:4d}\t{1:1.3f}\t{2:1.3f}\t{3:1.3f}\t{4:1.3f}\t{5:1.3f}'.format(
+                i, params[0], params[1], params[2], params[4], cost))   
+        elif len(params) == 4:
+            print('{0:4d}\t{1:1.3f}\t{2:1.3f}\t{3:1.3f}\t{4:1.3f}\t{5:1.3f}\t{6:1.3f}\t{7:1.3f}'.format(
+                i, params[0], params[1], params[2], params[4], params[6], params[8], cost))   
+        elif len(params) == 5:
+            print('{0:4d}\t{1:1.3f}\t{2:1.3f}\t{3:1.3f}\t{4:1.3f}\t{5:1.3f}\t{6:1.3f}\t{7:1.3f}\t{8:1.3f}\t{9:1.3f}'.format(
+                i, params[0], params[1], params[2], params[4], params[6], params[8], params[10], params[12], cost))   
