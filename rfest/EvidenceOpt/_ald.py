624d8fc26382b4a0933a56ce96462add0ba2367d
Ziwei Huang <huang-ziwei@outlook.com>
2019-10-16 10:12:23 +0200

    Update _ald.py

--- rfest/EvidenceOpt/_ald.py
+++ rfest/EvidenceOpt/_ald.py
@@ -78,12 +78,12 @@ class ALD(EmpiricalBayes):
 
     def print_progress(self, i, params, cost):
      
-        if len(params) == 3:
+        if len(params) == 6:
             print('{0:4d}\t{1:1.3f}\t{2:1.3f}\t{3:1.3f}\t{4:1.3f}\t{5:1.3f}'.format(
                 i, params[0], params[1], params[2], params[4], cost))   
-        elif len(params) == 4:
+        elif len(params) == 10:
             print('{0:4d}\t{1:1.3f}\t{2:1.3f}\t{3:1.3f}\t{4:1.3f}\t{5:1.3f}\t{6:1.3f}\t{7:1.3f}'.format(
                 i, params[0], params[1], params[2], params[4], params[6], params[8], cost))   
-        elif len(params) == 5:
+        elif len(params) == 14:
             print('{0:4d}\t{1:1.3f}\t{2:1.3f}\t{3:1.3f}\t{4:1.3f}\t{5:1.3f}\t{6:1.3f}\t{7:1.3f}\t{8:1.3f}\t{9:1.3f}'.format(
                 i, params[0], params[1], params[2], params[4], params[6], params[8], params[10], params[12], cost))   
