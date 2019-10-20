268a641b1d63385e2e28897a72cf6725968a1524
Ziwei Huang <huang-ziwei@outlook.com>
2019-10-20 17:11:28 +0200

    Update _asd.py

--- rfest/EvidenceOpt/_asd.py
+++ rfest/EvidenceOpt/_asd.py
@@ -28,14 +28,14 @@ class ASD(EmpiricalBayes):
         if len(self.dims) == 1:
             C, C_inv = C_t, C_t_inv
 
-        elif 1 < len(self.dims) < 3:
+        elif len(self.dims) ==2:
             delta_space = params[3]
             C_s, C_s_inv = self._make_1D_covariance(delta_space, self.dims[1])
 
             C = rho * np.kron(C_t, C_s)
             C_inv = (1 / rho) * np.kron(C_t_inv, C_s_inv)  
 
-        elif len(self.dims) > 2:
+        elif len(self.dims) ==3:
 
             delta_spacey = params[3]
             delta_spacex = params[4]
