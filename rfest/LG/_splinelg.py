d85663c045300654aee52e665cde999fb07afc1e
Ziwei Huang <huang-ziwei@outlook.com>
2019-10-21 17:10:53 +0200

    Update _splinelg.py

--- rfest/LG/_splinelg.py
+++ rfest/LG/_splinelg.py
@@ -30,7 +30,7 @@ class splineLG:
         S = self._make_splines_matrix(df_splines)
         self.S = S
         self.n_spline_coeff = self.S.shape[1]
-        self.w_spl = S @ np.linalg.inv(S.T @ X.T @ X @ S) @ S.T @ X.T @ y
+        self.w_spl = S @ np.linalg.solve(S.T @ X.T @ X @ S, S.T @ X.T @ y)
         
     def _make_splines_matrix(self, df):
         
