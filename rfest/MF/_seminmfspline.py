5e7f65269b16ee46302e93a8357493f5ac71fe49
Ziwei Huang <huang-ziwei@outlook.com>
2019-10-25 20:53:22 +0200

    Update _seminmfspline.py

--- rfest/MF/_seminmfspline.py
+++ rfest/MF/_seminmfspline.py
@@ -45,7 +45,7 @@ class SemiNMFSpline:
 
         if self.ndim == 1:
 
-            self.S = patsy.cr(np.arange(self.dims[0]), df[0])
+            S = patsy.cr(np.arange(self.dims[0]), df[0])
 
         elif self.ndim == 2:
 
