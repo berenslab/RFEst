2799773fb1746b3e32204dbf156bb7921ee07b56
Ziwei Huang <huang-ziwei@outlook.com>
2019-10-25 11:17:22 +0200

    fix bugs

--- rfest/__init__.py
+++ rfest/__init__.py
@@ -1,4 +1,4 @@
 from .GLM import *
 from .EvidenceOpt import *
 from ._make_data import make_data
-from ._utils import get_sdm, get_spatial_and_time_filters
+from ._utils import get_stimulus_desgin_matrix, get_spatial_and_time_filters
