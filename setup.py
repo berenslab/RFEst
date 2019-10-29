eb65ebc37fd26fad334c4790e0de559e374c6e1e
Ziwei Huang <huang-ziwei@outlook.com>
2019-10-29 14:00:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name='RFEst',
-      version='0.0.2',
-      description='Python 3 tool for receptive field estimation',
+      version='0.1.0',
+      description='Python 3 toolbox for receptive field estimation',
       author='Ziwei Huang',
       author_email='huang-ziwei@outlook.com',
       packages=find_packages(),
