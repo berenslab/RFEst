a32a455d06d35004be1728ab27c5c1f60465e09f
Ziwei Huang <huang-ziwei@outlook.com>
2020-01-24 10:46:29 +0100

    update

--- rfest/GLM/_splinelg.py
+++ rfest/GLM/_splinelg.py
@@ -33,8 +33,11 @@ class splineLG(splineBase):
         mse = np.sum((y - XS @ b)**2) / len(y)
 
         if self.lambd:
-            l1 = np.sum(np.abs(b))
-            l2 = np.sqrt(np.sum(b**2))           
+            # l1 = np.sum(np.abs(b))
+            # l2 = np.sqrt(np.sum(b**2))           
+            
+            l1 = np.linalg.norm(b, 1)
+            l2 = np.linalg.norm(b, 2)
             mse += self.lambd * ((1 - self.alpha) * l2 + self.alpha * l1) 
     
         return mse       
