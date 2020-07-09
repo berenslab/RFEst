18f9da6adfd36bebfe2616b19f7fc98dff5e5008
Ziwei Huang <huang-ziwei@outlook.com>
2020-07-09 19:57:52 +0200

    v1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='RFEst',
-      version='1.0.0',
+      version='1.1.0',
       description='Python 3 toolbox for receptive field estimation',
       author='Ziwei Huang',
       author_email='huang-ziwei@outlook.com',
