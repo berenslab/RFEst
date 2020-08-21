74f6bc31fccb0caa09933cb44d32748c0f3ffc04
Ziwei Huang <huang-ziwei@outlook.com>
2020-08-21 12:49:36 +0200

    fixed

--- rfest/simulate.py
+++ rfest/simulate.py
@@ -2,38 +2,38 @@ import numpy as np
 import scipy.signal
 import scipy.stats
 
-from ._utils import build_design_matrix, norm
+from ._utils import build_design_matrix, uvec
 
 def gaussian1d(dim=200, std=15):
-    return norm(scipy.signal.gaussian(dim, std=std))
+    return uvec(scipy.signal.gaussian(dim, std=std))
 
 def gaussian2d(dims=[25,25], std=[2,2]):
     gaussian_x = gaussian1d(dims[0], std=std[0])    
     gaussian_y = gaussian1d(dims[1], std=std[1]) 
-    return norm(np.kron(gaussian_x, gaussian_y)).reshape(dims)
+    return uvec(np.kron(gaussian_x, gaussian_y)).reshape(dims)
 
 def gaussian3d(dims, std):
     gaussian_t = np.gradient(gaussian1d(dims[0], std[0]))
     gaussian_s = gaussian2d(dims[1:], std[1:]).flatten()
-    return norm(np.kron(gaussian_t, gaussian_s)).reshape(dims)
+    return uvec(np.kron(gaussian_t, gaussian_s)).reshape(dims)
 
 def mexicanhat1d(dims=200, std=15, a=0.8):
     g0 = gaussian1d(dims, std)
     g1 = gaussian1d(dims, std*a)
     m = g1 - 0.65 * g0
-    return norm(m)
+    return uvec(m)
 
 def mexicanhat2d(dims=[25,25], std=[3,3], a=0.55):
     g0 = gaussian2d(dims, std)
     g1 = gaussian2d(dims, np.array(std) * a)
     m = g1 - 0.65 * g0
-    return norm(m)
+    return uvec(m)
 
 def mexicanhat3d(dims, std, a=0.3):
     g_t = np.gradient(gaussian1d(dims[0], std[0]))
     m_s = mexicanhat2d(dims[1:], std[1:], a).flatten()
     m = np.kron(g_t, m_s)  
-    return norm(m).reshape(dims)
+    return uvec(m).reshape(dims)
 
 def gabor2d(dims=[25, 25], omega=0.5, theta=np.pi/6, func=np.cos, K=1.):
     radius = (int(dims[1]/2.0), int(dims[0]/2.0))
@@ -46,13 +46,13 @@ def gabor2d(dims=[25, 25], omega=0.5, theta=np.pi/6, func=np.cos, K=1.):
     sinusoid = func(omega * x1) * np.exp(K**2 / 2)
     gabor = gauss * sinusoid
     
-    return norm(gabor)[:dims[0], :dims[1]]
+    return uvec(gabor)[:dims[0], :dims[1]]
 
 def gabor3d(dims, std, omega, theta, func=np.cos, K=np.pi):
     g_t = np.gradient(gaussian1d(dims[0], std))
     g_s = gabor2d(dims[1:], omega, theta, func, K).flatten()
     g = np.kron(g_t, g_s)  
-    return norm(g).reshape(dims)    
+    return uvec(g).reshape(dims)    
 
 def V1complex_2d(dims=[30, 40], scale=[.025, .03]):
 
@@ -63,8 +63,8 @@ def V1complex_2d(dims=[30, 40], scale=[.025, .03]):
 
     kt1 = scipy.stats.gamma.pdf(-tt, dims[0]/7.5, scale=scale[0])
     kt2 = scipy.stats.gamma.pdf(-tt, dims[1]/6, scale=scale[1])
-    kt1 /= np.linalg.norm(kt1)
-    kt2 /= -np.linalg.norm(kt2)
+    kt1 /= np.linalg.uvec(kt1)
+    kt2 /= -np.linalg.uvec(kt2)
 
     kt = np.vstack([kt1, kt2]).T
 
@@ -73,14 +73,14 @@ def V1complex_2d(dims=[30, 40], scale=[.025, .03]):
     kx1 = np.cos(2*np.pi*xx/2 + np.pi/5) * np.exp(-1/(2*0.35**2) * xx**2)
     kx2 = np.sin(2*np.pi*xx/2 + np.pi/5) * np.exp(-1/(2*0.35**2) * xx**2)
 
-    kx1 /= np.linalg.norm(kx1)
-    kx2 /= np.linalg.norm(kx2)
+    kx1 /= np.linalg.uvec(kx1)
+    kx2 /= np.linalg.uvec(kx2)
 
     kx = np.vstack([kx1, kx2])
 
     k = kt @ kx
     
-    return norm(k)
+    return uvec(k)
 
 def get_stimulus(n_samples, dims, kind='3dnoise', delta=1000, random_seed=1990):
 
@@ -110,19 +110,19 @@ def get_stimulus(n_samples, dims, kind='3dnoise', delta=1000, random_seed=1990):
     if len(dims) == 1:
         
         Sigma = kernel(dims[0], delta)
-        Stim = np.random.multivariate_normal(np.zeros(len(Sigma)), Sigma, n_samples) 
+        Stim = np.random.multivariate_uvecal(np.zeros(len(Sigma)), Sigma, n_samples) 
         X = Stim
 
     elif len(dims) == 2 and kind=='2dbar':
 
         Sigma = kernel(dims[1], delta)
-        Stim = np.random.multivariate_normal(np.zeros(len(Sigma)), Sigma, n_samples)
+        Stim = np.random.multivariate_uvecal(np.zeros(len(Sigma)), Sigma, n_samples)
         X = build_design_matrix(Stim, dims[0])
 
     elif len(dims) == 2 and kind=='2dnoise':
         
         Sigma = np.kron(kernel(dims[0], delta), kernel(dims[1], delta))
-        Stim = np.random.multivariate_normal(np.zeros(len(Sigma)), Sigma, n_samples) 
+        Stim = np.random.multivariate_uvecal(np.zeros(len(Sigma)), Sigma, n_samples) 
         X = Stim
 
     elif len(dims) == 3 and kind=='3dnoise':
