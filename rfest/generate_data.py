e7115b2c58cb585bf211bca1777522c7cbabbd7f
jonathanoesterle <jonathan.oesterle1@gmail.com>
2022-05-20 18:45:29 +0200

    Improved GLM tests.

--- rfest/generate_data.py
+++ rfest/generate_data.py
@@ -12,7 +12,7 @@ def generate_2d_rf_data(noise='white'):
     dt = 1.
 
     X = flickerbar(n_samples=1000, dims=dims, design_matrix=True, random_seed=2046, beta=beta)
-    y = get_response(X, w_true.flatten(), dt=dt)
+    y = get_response(X, w_true.flatten(), dt=dt, distr='none')
 
     return w_true, X, y, dims, dt
 
@@ -24,19 +24,18 @@ def generate_3d_rf_data(noise='white'):
 
     w_true = np.stack(
         [-0.1 * wframe, -0.5 * wframe, 0.1 * wframe, 0.5 * wframe, 1 * wframe * 0.5 * wframe, 0.1 * wframe])
-    w_true += np.random.normal(0, np.max(wframe) * 0.01, w_true.shape)
 
     dims = w_true.shape
     dt = 1.
 
     X = noise2d(n_samples=1000, dims=dims[1:], design_matrix=True, random_seed=2046, beta=beta)
-    y = get_response(X, w_true, dt=dt)
+    y = get_response(X, w_true, dt=dt, distr='none')
 
     return w_true, X, y, dims, dt
 
 
 def generate_spike_train(noise='white'):
-    # TODO: so far the response data is completly independent from the stimulus
+    # TODO: so far the response data is completely independent from the stimulus
     beta = None if noise == 'white' else 1
 
     w_true = V1complex_2d()[23:30, 14:22]
